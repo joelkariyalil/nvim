8deb7a42ef55706f6f2312c8ad558b9e1be4578c
alan.devine <alanrdevine@gmail.com>
2025-01-14 23:30:46 +0000

    fixes trailing ')'

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,10 +26,10 @@ return {
 
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc="Find Files"})
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {desc="File history"})
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", {desc="Live grep"})
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", {desc="Grep String"})
-		keymap.set("n", "<leader>fp", require("telescope.builtin").diagnostics, { desc = "Telescope Diagnostics" }))
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "File history" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Grep String" })
+		keymap.set("n", "<leader>fp", require("telescope.builtin").diagnostics, { desc = "Telescope Diagnostics" })
 	end,
 }
