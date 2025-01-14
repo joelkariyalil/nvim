35d371ecaf24a73844fa9f6a6bfb01b396796497
alan.devine <alanrdevine@gmail.com>
2025-01-14 12:54:56 +0000

    adds bind for adding go tests

--- lua/config/core/languages/go.lua
+++ lua/config/core/languages/go.lua
@@ -34,6 +34,7 @@ end
 vim.keymap.set("n", "<leader>gie", "<cmd>GoIfErr<CR>", { desc = "Adds go if error boilerplate" })
 vim.keymap.set("n", "<leader>gatj", "<cmd>GoAddTag json<CR>", { desc = "Adds json tags to struct" })
 vim.keymap.set("n", "<leader>gaty", "<cmd>GoAddTag yaml<CR>", { desc = "Adds yaml tags to struct" })
-vim.keymap.set("n", "<leader>gfs", "<cmd>GoFillStruct<CR>", { desc = "Adds yaml tags to struct" })
+vim.keymap.set("n", "<leader>gfs", "<cmd>GoFillStruct<CR>", { desc = "Fills Struct" })
+vim.keymap.set("n", "<leader>gaat", "<cmd>GoAddAllTest<CR>", { desc = "Adds tests file" })
 
 vim.filetype.add({ extension = { templ = "templ" } })
