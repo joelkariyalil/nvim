96fcf552fa2b01971514d12f9bd78d24d1f8c395
alan.devine <alanrdevine@gmail.com>
2025-01-14 12:56:01 +0000

    adds diagnostics bind + docs on binds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,9 +26,10 @@ return {
 
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc="Find Files"})
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {desc="File history"})
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", {desc="Live grep"})
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", {desc="Grep String"})
+		keymap.set("n", "<leader>fp", require("telescope.builtin").diagnostics, { desc = "Telescope Diagnostics" }))
 	end,
 }
