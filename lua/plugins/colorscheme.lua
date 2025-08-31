1c88fdb82374fe6b023e916788f5a1bdb98007c8
chackoj <joel.chacko@autodesk.com>
2025-09-01 00:05:32 +0100

    MOD: Changed theme to Dark night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,32 @@
 return {
-	"rmehri01/onenord.nvim",
+	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
 
 	config = function()
-		vim.cmd("colorscheme onenord")
+		require("gruvbox").setup({
+			terminal_colors = true,
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true,
+			contrast = "hard", -- High contrast setting
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = false,
+		})
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
