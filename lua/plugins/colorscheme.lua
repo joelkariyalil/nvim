fad102a52d6f83a3141f519b90ac0f9025898290
JOEL CHACKO <joel.chacko@autodesk.com>
2025-09-10 22:14:52 +0100

    Revert "MOD: Changed theme to Dark night"
    This reverts commit 1c88fdb82374fe6b023e916788f5a1bdb98007c8.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,8 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"rmehri01/onenord.nvim",
 	priority = 1000,
 
 	config = function()
-		require("gruvbox").setup({
-			terminal_colors = true,
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = true,
-				emphasis = true,
-				comments = true,
-				operators = false,
-				folds = true,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true,
-			contrast = "hard", -- High contrast setting
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = false,
-		})
-		vim.cmd("colorscheme gruvbox")
+		vim.cmd("colorscheme onenord")
 	end,
 }
