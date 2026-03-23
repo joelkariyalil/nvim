3709e25149361244807cf6e060d6a4c9cf26017d
chackoj <joelkariyalil@gmail.com>
2026-03-23 18:43:16 +0000

    Reverting global git config change

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,7 +1,3 @@
-if vim.env.GIT_CONFIG_GLOBAL == nil then
-	vim.env.GIT_CONFIG_GLOBAL = ""
-end
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
